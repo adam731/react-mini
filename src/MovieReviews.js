e2515d3849247a79758f819c0690e971aefbd57b
Adam <adam3417@gmail.com>
2023-02-19 23:33:14 -0330

    adding / beta testing useState

--- src/MovieReviews.js
+++ src/MovieReviews.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { CardGroup, Card } from "react-bootstrap";
 import moviesData from "./data/movies.json";
 
 function MovieReviews() {
+  const [movieReviews, setMovieReviews] = useState(moviesData);
+
   return (
     <div className="d-flex">
       <CardGroup>
-        {moviesData.map((movie) => (
+        {movieReviews.map((movie) => (
           <Card key={movie.id}>
-            <Card.Img variant="top" src={movie.image} style={{ height: "250px", objectFit: "cover" }} />
+            <Card.Img
+              variant="top"
+              src={movie.image}
+              style={{ height: "250px", objectFit: "cover" }}
+            />
             <Card.Body>
               <Card.Title>{movie.name}</Card.Title>
               <Card.Text>{movie.releaseDate}</Card.Text>
